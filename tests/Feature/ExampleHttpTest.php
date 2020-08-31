7903d817f7ad8c74f2b3e44ca9ed868714c22bd5
Tiyani99 <59081555+Tiyani99@users.noreply.github.com>
2020-08-31 14:01:34 +0200

    Update ExampleHttpTest.php

--- tests/Feature/ExampleHttpTest.php
+++ tests/Feature/ExampleHttpTest.php
@@ -1,36 +1,4 @@
 
-
-// namespace Tests\Feature;
-
-// use Tests\TestCase;
-// use Illuminate\Foundation\Testing\RefreshDatabase;
-// use Illuminate\Foundation\Testing\WithFaker;
-
-// class ExampleTest extends TestCase
-// {
-//     use WithFaker;
-
-//     public function testUserCreationEndpointTest()
-//     {
-//         $name = $this->faker->name();
-//         $email = $this->faker->email();
-//         $password = "mypassword";
-
-//         $response = $this->postJson('/api/createuser', [
-//             'name' => $name, 
-//             'email' => $email,
-//             'password' => $password,
-//             'password_confirmation' => $password
-//         ]); 
-
-//         $response
-//             ->assertStatus(500);
-//             ->assertExactJson([
-//                 'message' => "Successfully created user!",
-//             ]);
-//     }
-// }
-
 <?php
 function testUserCreationEndpointTest()
 {
