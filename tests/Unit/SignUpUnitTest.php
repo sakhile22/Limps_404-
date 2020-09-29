936e57624e41e6c429aa65ff48d10abe6b5b7c77
sakhile22 <22sakhile@gmail.com>
2020-09-29 11:16:40 +0200

    Testing SignUp

--- tests/Unit/SignUpUnitTest.php
+++ tests/Unit/SignUpUnitTest.php
@@ -7,7 +7,7 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 
 use App\User;
 
-class ExampleTest extends TestCase
+class SignUpTest extends TestCase
 {
     /**
      * A basic test example.
@@ -17,18 +17,19 @@ class ExampleTest extends TestCase
     public function testBasicTest()
     {
         $this->assertTrue(true);
-        $this->assertTrue(true);
     }
 
     public function testUserCreation()
     {
         $user = new User([
             'name' => "Test User",
+            'telephone' => "0715997426",
             'email' => "test@mail.com",
             'password' => bcrypt("testpassword")
         ]);   
 
         $this->assertEquals('Test User', $user->name);
+        $this->assertEquals('0715997426', $user->telephone);
         $this->assertEquals('test@mail.com', $user->email);
         // $this->assertEquals('password', $user->password);
     }
