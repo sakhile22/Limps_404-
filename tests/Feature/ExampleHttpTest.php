fe8e5ae1ae724b4330bded7702a6627b89b30179
Tiyani99 <59081555+Tiyani99@users.noreply.github.com>
2020-09-29 14:00:56 +0200

    Create ExampleHttpTest.php

---
+++ tests/Feature/ExampleHttpTest.php
@@ -0,0 +1,21 @@
+<?php
+function testUserCreationEndpointTest()
+{
+    $name = $this->faker->name();
+    $email = $this->faker->email();
+    $password = "mypassword";
+
+    $response = $this->postJson('/api/createuser', [
+        'name' => $name, 
+        'email' => $email,
+        'password' => $password,
+        'password_confirmation' => $password
+    ]); 
+
+    $response
+        ->assertStatus(201)
+        ->assertExactJson([
+            'message' => "Successfully created user!",
+        ]);
+
+}
