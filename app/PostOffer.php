e3caf6d623c5b3ba4dbfbbc730692e8b470bb3f8
sakhile22 <22sakhile@gmail.com>
2020-09-29 12:13:35 +0200

    Testing post offers page

--- app/PostOffer.php
+++ app/PostOffer.php
@@ -5,7 +5,7 @@ namespace App;
 use Illuminate\Notifications\Notifiable;
 // use Illuminate\Foundation\Auth\PostOffer as Authenticatable;
 
-class PostOffer
+class PostOffer extends \PHPUnit\Framework\TestCase
 {
     use Notifiable;
 
