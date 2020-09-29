9fedb94434d6f377dad2bdc0c3485cc2b6ad4843
sakhile22 <22sakhile@gmail.com>
2020-09-29 12:34:00 +0200

    Testing post offers page

--- tests/Unit/PostOfferUnitTest.php
+++ tests/Unit/PostOfferUnitTest.php
@@ -7,7 +7,7 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 
 use App\User;
 
-class SignUpTest extends TestCase
+class PostOfferTest extends TestCase
 {
     /**
      * A basic test example.
